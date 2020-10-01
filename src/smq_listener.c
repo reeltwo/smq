c49cd8a3af839223202e891d868aaed466d7ae51
rimim <nomail@thanks.com>
2020-10-01 16:25:30 -0400

    Disable stdout buffering and parse json messages in listener

--- src/smq_listener.c
+++ src/smq_listener.c
@@ -4,7 +4,39 @@
 
 static void message_callback(const char* topic_name, const uint8_t* msg, size_t len, void* arg)
 {
-    printf("%s : %s\n", topic_name, msg);
+    json_tokener* tok = json_tokener_new();
+    json_object* jobj = json_tokener_parse_ex(tok, (const char*)msg, len);
+    if (jobj != NULL)
+    {
+        json_object_object_foreach(jobj, key, val)
+        {
+            int val_type = json_object_get_type(val);
+            switch (val_type)
+            {
+                case json_type_null:
+                    printf("%s:NULL\n", key);
+                    break;
+                case json_type_boolean:
+                    printf("%s:%s\n", key, json_object_get_boolean(val) ? "true": "false");
+                    break;
+                case json_type_double:
+                    printf("%s:%g\n", key, json_object_get_double(val));
+                    break;
+                case json_type_int:
+                    printf("%s:%d\n", key, json_object_get_int(val));
+                    break;
+                case json_type_string:
+                    printf("%s:\"%s\"\n", key, json_object_get_string(val));
+                    break;
+                case json_type_object:
+                    break;
+                case json_type_array:
+                    // Support int array as byte-buffer
+                break;
+            }
+        }
+    }
+    json_tokener_free(tok);
 }
 
 int main(int argc, const char* argv[])
@@ -17,6 +49,7 @@ int main(int argc, const char* argv[])
         fprintf(stderr, "%s: <topic>\n", progname);
         return 1;
     }
+    setvbuf(stdout, NULL, _IONBF, 0);
     /* Initialize dzmq */
     if (!smq_init()) return 1;
 
